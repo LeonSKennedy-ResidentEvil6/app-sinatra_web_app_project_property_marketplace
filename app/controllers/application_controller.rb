42400c965a7df185e556b27c5033fb04816d4aaf
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-10-14 16:21:01 -0700

    edited files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,50 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+ 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret_properties"
   end
 
+  # done/tested/working
   get "/" do
-    erb :welcome
+    if is_logged_in?
+      redirect '/properties'
+    else 
+      erb :welcome
+    end 
   end
+
+  # check if user is logged in
+  def is_logged_in?
+    !!current_user # double negation returns true || false value
+  end 
+
+  # look up current user stored in session using id
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end 
+
+  def login
+    # check if the user knows the username && password can be authenticated
+    # if so, set the session
+    # otherwise, user is redirect to login page
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/properties'
+    else 
+      # can add flash extention to generate error message
+      "unable to find this user in the system"
+      redirect '/login'
+    end 
+  end 
+
+  def logout!
+    session.clear
+  end 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,85 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
-  end
+    # done
+    get "/signup" do
+      if is_logged_in?
+        redirect '/properties'
+      else 
+        erb :"/users/signup"
+      end 
+    end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
-  end
+    # done
+    # create a new user
+    post '/signup' do
+      @user = User.new(
+        first_name: params[:first_name], 
+        last_name: params[:last_name], 
+        username: params[:username], 
+        email: params[:email], 
+        password: params[:password]
+      )
+      @user.seller = params[:seller] == "yes" ? true : false # new user select to be a seller or buyer
+      if @user.save
+        session[:user_id] = @user.id
+        redirect '/properties'
+      else 
+        # can use flash extention to give error messgae instead of printing string
+        "please enter all required info"
+        redirect '/signup'
+      end 
+    end 
+
+    # done
+    # current logged user can come back and view properties without re-login, otherwise, user needs log in
+    get '/login' do
+      if is_logged_in?
+        redirect '/properties'
+      else 
+        erb :"/users/login"
+      end 
+    end   
+    
+    # done 
+    # check if user's login info matches what is stored in session using login method defined in app controller
+    post '/login' do 
+       login
+    end 
+
+    # user can log out from account
+    get '/logout' do
+      if is_logged_in?
+        logout! # method defined in app controller
+        # can add flash extension to generate message
+        "you have successfully logged. see you later!"
+      else 
+        redirect '/'
+      end 
+    end 
+
+    # done
+    # user can view all buyers and sellers
+    # ideally, buyers can only see sellers && sellers can only see buyers
+    get '/users' do 
+      @sellers = User.where(seller: true)
+      @buyers = User.where(seller: false)
+      if is_logged_in?
+        erb :"/users/index"
+      else 
+        # can add flash extention to generate error message here
+        redirect '/login'
+      end 
+    end 
+
+      # continue working the following!!!!
+
+      # user can view properties they have offered to buy && offer status 
+    get '/offers' do
+      if is_logged_in?
+        @offer = 
+      end 
+    end 
+
 
   # POST: /users
   post "/users" do
